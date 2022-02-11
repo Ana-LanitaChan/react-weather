cef561555a9794281d2e5c3df6346183d1a98984
Ana-LanitaChan <85143407+Ana-LanitaChan@users.noreply.github.com>
2022-02-11 13:31:09 -0600

    Delete useStates from search
    Temporal solution*

--- src/Search.js
+++ src/Search.js
@@ -1,28 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Search.css";
 
 export default function Search() {
-  let [cityvalue, setCityvalue] = useState("");
-  let [display, setCitydisplay] = useState("");
-
-  function handleSubmit(event) {
-    event.preventDefault();
-  }
-
-  function updateCity(event) {
-    setCityvalue(event.target.value);
-  }
-
   return (
     <div className="Search">
-      <form action="search" class="mb-3" id="form" onSubmit={handleSubmit}>
+      <form action="search" class="mb-3" id="form">
         <div class="input-group">
           <input
             class="form-control border border-dark"
             type="search"
             placeholder="Search for a city"
             id="input-search"
-            onChange={updateCity}
           />
           <input
             class="btn btn-dark submit-style"
